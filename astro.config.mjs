d87df1c31b4bbe3cf1859c0e16e3cbabf0b6efc5
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2023-02-04 12:27:25 -0800

    Start SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://adapt.chat',
   integrations: [
     tailwind({
       config: { path: 'tailwind.config.cjs', applyBaseStyles: false },
     }),
+    sitemap(),
   ],
 });
