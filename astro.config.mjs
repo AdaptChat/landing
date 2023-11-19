a9efee8d9d507d06424ada7937e8082a088fe526
jay3332 <40323796+jay3332@users.noreply.github.com>
2023-11-19 16:59:24 -0500

    cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,8 +12,12 @@ export default defineConfig({
   site: 'https://adapt.chat',
   integrations: [
     tailwind({
-      config: { path: 'tailwind.config.cjs', applyBaseStyles: false },
+      config: {
+        path: 'tailwind.config.cjs',
+        applyBaseStyles: false
+      }
     }),
     sitemap(),
   ],
+  adapter: cloudflare()
 });
